c0114dac4f30170a093f4e14303e7ba04bbe4138
m43i <36700796+m43i@users.noreply.github.com>
2023-10-28 17:22:00 +0200

    (chore): update to next@14 for stable server actions

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -3,11 +3,7 @@ import "./src/env.mjs";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    experimental: {
-        serverActions: true,
-    },
     transpilePackages: ["@energyleaf/ui"],
-}
+};
 
 export default nextConfig;
-
