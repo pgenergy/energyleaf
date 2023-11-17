8dd4decbcf63b3abc85c0e3becbc2c89fecf2a0e
Daniel Tapken <danieltapken32@gmail.com>
2023-11-17 15:22:28 +0100

    Allow to search by all columns

--- apps/web/src/app/(app)/(site)/devices/page.tsx
+++ apps/web/src/app/(app)/(site)/devices/page.tsx
@@ -1,20 +1,40 @@
 import { Card, CardContent, CardDescription, CardHeader, CardTitle, Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@energyleaf/ui";
 import { getSession } from "@/lib/auth/auth";
-import { redirect, usePathname } from "next/navigation";
+import { redirect } from "next/navigation";
 import { getDevicesByUser } from "@/query/device";
 import { SortOrder } from "@energyleaf/db/util";
-import { Button } from "react-day-picker";
-import router, { Router, useRouter } from "next/router";
 import DeviceSortButton from "@/components/devices/device-sort-button";
+import { device } from "@energyleaf/db/schema";
+import { format } from "date-fns";
+import de from "date-fns/locale/de"
 
-export default async function DevicesPage({ searchParams }: { searchParams: { sortOrder: SortOrder } }) {
+export default async function DevicesPage({ searchParams }: { searchParams: { sortOrder: SortOrder, sortProp: String } }) {
     const session = await getSession()
     if (!session) {
         redirect("/");
     }
 
     const sortOrder = searchParams?.sortOrder ? searchParams.sortOrder : SortOrder.ASC
-    const devices = await getDevicesByUser(session.user.id, sortOrder)
+    
+    var sortProp: (x: typeof device) => any = x => x.name
+    if (searchParams?.sortProp) {
+        const prop = Object.keys(device).find(x => x === searchParams.sortProp)
+        if (prop) {
+            sortProp = x => x[prop]
+        }
+    }
+
+    const devices = await getDevicesByUser(session.user.id, sortOrder, sortProp)
+
+    const dateString = (created: Date | null) => {
+        if (!created) {
+            return ""
+        }
+
+        return `${format(created, "PPpp", {
+            locale: de,
+        })}`;
+    };
 
     return (
         <div className="flex flex-col gap-4">
@@ -26,16 +46,22 @@ export default async function DevicesPage({ searchParams }: { searchParams: { so
                 <CardContent>
                     { (devices && devices.length > 0) ? (
                         <Table>
-                            <TableHead>
-                                <TableHeader>
-                                    <DeviceSortButton sortOrder={sortOrder}>Gerätename</DeviceSortButton>
-                                </TableHeader>
-                            </TableHead>
+                            <TableHeader>
+                                <TableHead>
+                                <DeviceSortButton propName="name" sortOrder={searchParams.sortProp === "name" ? sortOrder : null}/>
+                                </TableHead>
+                                <TableHead>
+                                    <DeviceSortButton propName="created" sortOrder={searchParams.sortProp === "created" ? sortOrder : null}/>
+                                </TableHead>
+                            </TableHeader>
                             <TableBody>
                                 { devices.map((device) => (
                                     <TableRow>
                                         <TableCell>
-                                            {device}
+                                            {device.name}
+                                        </TableCell>
+                                        <TableCell>
+                                            {dateString(device.created)}
                                         </TableCell>
                                     </TableRow>
                                 ))}
--- apps/web/src/components/devices/device-sort-button.tsx
+++ apps/web/src/components/devices/device-sort-button.tsx
@@ -1,25 +1,42 @@
 'use client';
 
 import { SortOrder } from "@energyleaf/db/util";
-import { Button } from "@energyleaf/ui";
+import { ArrowDown, ArrowUp } from "lucide-react";
 import { usePathname, useRouter } from "next/navigation";
 
 interface Props {
-    sortOrder: SortOrder;
+    sortOrder: SortOrder | null;
+    propName: String;
 }
 
-export default function DeviceSortButton({ sortOrder }: Props) {
+export default function DeviceSortButton({ sortOrder, propName }: Props) {
     const router = useRouter();
     const pathname = usePathname();
 
-    function onClick(prevSortOrder: SortOrder, pathname: String) {
+    function onClick(prevSortOrder: SortOrder | null, pathname: string) {
+        if (!sortOrder) {
+            prevSortOrder = SortOrder.DESC;
+        }
+
         const search = new URLSearchParams({
-            sortOrder: prevSortOrder === SortOrder.ASC ? SortOrder.DESC : SortOrder.ASC
+            sortOrder: prevSortOrder === SortOrder.ASC ? SortOrder.DESC : SortOrder.ASC,
+            sortProp: propName.toString()
         });
         router.push(`${pathname}?${search.toString()}`);
     }
 
     return (
-        <Button onClick={() => onClick(sortOrder, pathname)}>Test</Button>
+        <div className="flex items-center cursor-pointer" onClick={() => onClick(sortOrder, pathname)}>
+            <span className="mr-2">Test</span>
+            {
+                sortOrder ? (sortOrder === SortOrder.ASC ? (
+                    <ArrowDown/>
+                ) : (
+                    <ArrowUp/>
+                )) : (
+                    <div/> // nothing
+                )
+            }
+        </div>
     )
 }
