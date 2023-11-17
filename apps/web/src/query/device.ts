2049e5cf2a68cda93fec887a5f87c74e7e54f152
Daniel Tapken <danieltapken32@gmail.com>
2023-11-17 15:21:01 +0100

    Add field created to device

--- apps/web/src/query/device.ts
+++ apps/web/src/query/device.ts
@@ -2,10 +2,11 @@ import { cache } from "react";
 
 import { getDevicesByUser as getDbDevicesByUser } from "@energyleaf/db/query";
 import { SortOrder } from "@energyleaf/db/util";
+import { device } from "@energyleaf/db/schema";
 
 /**
  * Cached query to retrieve the devices per user.
  */
-export const getDevicesByUser = cache(async (id: string, sortOrder: SortOrder) => {
-    return getDbDevicesByUser(Number(id), sortOrder);
+export const getDevicesByUser = cache(async (id: string, sortOrder: SortOrder, orderBy: (x: typeof device) => any) => {
+    return getDbDevicesByUser(Number(id), sortOrder, orderBy);
 });
--- packages/db/src/query/device.ts
+++ packages/db/src/query/device.ts
@@ -4,18 +4,18 @@ import db from "..";
 import { device } from "../schema";
 import { SortOrder } from "../util";
 
-export async function getDevicesByUser(userId: number, sortOrder: SortOrder = SortOrder.ASC) {
+export async function getDevicesByUser(userId: number, sortOrder: SortOrder = SortOrder.ASC, orderBy: (x: typeof device) => any = x => x.name) {
     const query = db.select().from(device).where(eq(device.userId, userId));
     if (sortOrder === SortOrder.ASC) {
-        query.orderBy(device.name);
+        query.orderBy(orderBy(device));
     }
     else {
-        query.orderBy(desc(device.name));
+        query.orderBy(desc(orderBy(device)));
     }
 
     const results = await query;
     if (results.length === 0) {
         return null;
     }
-    return results.map(x => x.name);
+    return results;
 }
