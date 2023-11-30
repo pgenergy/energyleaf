69ac514f1eb572eece7d0a77a8eb8797c246baac
Daniel Tapken <danieltapken32@gmail.com>
2023-11-30 17:05:39 +0100

    Implement update update of device in frontend

--- apps/web/src/components/devices/devices-tables.tsx
+++ apps/web/src/components/devices/devices-tables.tsx
@@ -5,20 +5,23 @@ import DeviceSortButton from "./device-sort-button";
 import { SortOrder } from "@energyleaf/db/util";
 import { de } from "date-fns/locale";
 import { format } from "date-fns";
-import React from "react";
-import { Pen, Trash } from "lucide-react";
+import React, { useState } from "react";
+import { Pen, Plus, Trash } from "lucide-react";
 import { deleteDevice } from "@/actions/device";
 import { toast } from "@energyleaf/ui/hooks";
+import DeviceDetailsDialog from "./device-details-dialog";
 
 interface Props {
     userId: string;
-    devices: { id: Number, name: String, created: Date | null }[];
+    devices?: { id: Number, name: String, created: Date | null }[] | null;
     sortOrder: SortOrder;
     sortProp: String;
 }
 
 export default function DevicesTable({ userId, devices, sortOrder, sortProp }: Props) {
-    console.log(devices)
+    const [detailsOpen, setDetailsOpen] = useState(false);
+    const [device, setDevice] = useState<{ id: Number; name: String; } | undefined>(undefined)
+
     const dateString = (created: Date | null) => {
         if (!created) {
             return ""
@@ -29,10 +32,10 @@ export default function DevicesTable({ userId, devices, sortOrder, sortProp }: P
         })}`;
     };
 
-    async function onDeleteButtonClick(device: { id: any; name?: String; created?: Date | null; }) {
+    async function onDeleteButtonClick(event: React.MouseEvent<HTMLButtonElement, MouseEvent>, device: { id: any; name?: String; created?: Date | null; }) {
+        event.stopPropagation()
         if (!confirm("Soll das Gerät wirklich gelöscht werden?")) {
             return
-            
         }
 
         try {
@@ -50,45 +53,72 @@ export default function DevicesTable({ userId, devices, sortOrder, sortProp }: P
         }
     }
 
+    function onAddClick() {
+        setDevice(undefined)
+        setDetailsOpen(true)
+    }
+
+    function onEditClick(device: { id: Number; name: String; }) {
+        setDevice(device)
+        setDetailsOpen(true)
+    }
+
     return (
-        <Table>
-            <TableHeader>
-                <TableHead>
-                    <DeviceSortButton propName="name" sortOrder={sortProp === "name" ? sortOrder : null}>
-                        Gerätename
-                    </DeviceSortButton>
-                </TableHead>
-                <TableHead>
-                    <DeviceSortButton propName="created" sortOrder={sortProp === "created" ? sortOrder : null}>
-                        Erstelldatum
-                    </DeviceSortButton>
-                </TableHead>
-                <TableHead>
-                    Aktionen
-                </TableHead>
-            </TableHeader>
-            <TableBody>
-                { devices.map((device) => (
-                    <TableRow key={crypto.randomUUID()}>
-                        <TableCell>
-                            {device.name}
-                        </TableCell>
-                        <TableCell>
-                            {dateString(device.created)}
-                        </TableCell>
-                        <TableCell>
-                            <div className="flex space-x-2">
-                                <Button className="w-7 h-7 rounded-full bg-primary p-1">
-                                    <Pen className="w-4 h-4" />
-                                </Button>
-                                <Button className="w-7 h-7 rounded-full bg-primary p-1" onClick={() => onDeleteButtonClick(device)}>
-                                    <Trash className="w-4 h-4" />
-                                </Button>
-                            </div>
-                        </TableCell>
-                    </TableRow>
-                ))}
-            </TableBody>
-        </Table>
+        <div>
+            <DeviceDetailsDialog userId={userId} open={detailsOpen} setOpen={setDetailsOpen} device={device}/>
+            <div className="flex justify-end">
+                <Button onClick={onAddClick}>
+                    <Plus className="mr-2"/>
+                    Gerät hinzufügen
+                </Button>
+            </div>
+            {(devices && devices.length > 0) ? (
+                <div>
+                    <Table>
+                        <TableHeader>
+                            <TableHead>
+                                <DeviceSortButton propName="name" sortOrder={sortProp === "name" ? sortOrder : null}>
+                                    Gerätename
+                                </DeviceSortButton>
+                            </TableHead>
+                            <TableHead>
+                                <DeviceSortButton propName="created" sortOrder={sortProp === "created" ? sortOrder : null}>
+                                    Erstelldatum
+                                </DeviceSortButton>
+                            </TableHead>
+                            <TableHead>
+                                Aktionen
+                            </TableHead>
+                        </TableHeader>
+                        <TableBody>
+                            { devices.map((device) => (
+                                <TableRow key={crypto.randomUUID()} className="cursor-pointer" onClick={() => onEditClick(device)}>
+                                    <TableCell>
+                                        {device.name}
+                                    </TableCell>
+                                    <TableCell>
+                                        {dateString(device.created)}
+                                    </TableCell>
+                                    <TableCell>
+                                        <div className="flex space-x-2">
+                                            <Button className="w-7 h-7 rounded-full bg-primary p-1" onClick={() => onEditClick(device)}>
+                                                <Pen className="w-4 h-4" />
+                                            </Button>
+                                            <Button className="w-7 h-7 rounded-full p-1" onClick={e => onDeleteButtonClick(e, device)}>
+                                                <Trash className="w-4 h-4" />
+                                            </Button>
+                                        </div>
+                                    </TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </div>
+            ) : (
+                <div className="flex flex-row justify-center">
+                    <p className="text-muted-foreground">Noch keine Geräte vorhanden</p>
+                </div>
+            )}
+        </div>
     )
 }
