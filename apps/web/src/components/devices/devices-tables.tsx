f34b403827c0eb4edadfcfdd810560d81abd2225
Daniel Tapken <danieltapken32@gmail.com>
2023-12-14 09:09:10 +0100

    Fix bug that edit dialog was opened when device was deleted

--- apps/web/src/components/devices/devices-tables.tsx
+++ apps/web/src/components/devices/devices-tables.tsx
@@ -38,7 +38,7 @@ export default function DevicesTable({ userId, devices, sortOrder, sortProp }: P
         event: React.MouseEvent<HTMLButtonElement>,
         id: number,
     ) {
-        event.preventDefault();
+        event.stopPropagation(); // otherwise, the row click event is triggered and the edit dialog is opened
         const delDevice = devices?.find((x) => x.id === id);
         if (!delDevice) {
             return;
