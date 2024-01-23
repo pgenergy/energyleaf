e1d571f17a9dcd8bbebe2a70e7c7d4b53ceecc16
Jessy Tieu <jessytieu@gmx.de>
2024-01-23 13:15:13 +0100

    SensorID aus Registrierung entfernt

--- apps/web/src/actions/auth.ts
+++ apps/web/src/actions/auth.ts
@@ -15,7 +15,7 @@ import { createUser, getUserByMail, type CreateUserType } from "@energyleaf/db/q
  * Server action for creating a new account
  */
 export async function createAccount(data: z.infer<typeof signupSchema>) {
-    const { mail, password, passwordRepeat, sensorId, username } = data;
+    const { mail, password, passwordRepeat, username } = data;
 
     if (password !== passwordRepeat) {
         throw new Error("Passwörter stimmen nicht überein.");
@@ -42,7 +42,6 @@ export async function createAccount(data: z.infer<typeof signupSchema>) {
         await createUser({
             email: mail,
             password: hash,
-            sensorId,
             username,
         } satisfies CreateUserType);
     } catch (_err) {
