7049e8476fc037e7638f18f96fbfb931f36f4225
m43i <36700796+m43i@users.noreply.github.com>
2024-02-05 16:09:09 +0100

    fix: do not error when user has no sensor, fix scroll bug

--- packages/ui/src/components/nav/sidebar.tsx
+++ packages/ui/src/components/nav/sidebar.tsx
@@ -2,6 +2,7 @@
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { ScrollArea } from "@radix-ui/react-scroll-area";
 import { default as clsx } from "clsx";
 
 interface NavLink {
@@ -19,23 +20,25 @@ export function Sidebar({ links }: Props) {
     const pathname = usePathname();
 
     return (
-        <nav className="max-h-[calc(100vh - 3.5rem)] fixed left-0 top-14 hidden w-[13%] flex-col gap-4 overflow-scroll py-8 md:flex">
-            {links.map((link) => (
-                <Link
-                    className={clsx(
-                        {
-                            "hover:bg-accent": pathname !== link.path,
-                            "bg-primary text-primary-foreground": pathname === link.path,
-                        },
-                        "flex flex-row items-center rounded-r-full px-4 py-2 font-medium",
-                    )}
-                    href={link.path}
-                    key={link.slug}
-                >
-                    {link.icon ?? null}
-                    {link.title}
-                </Link>
-            ))}
+        <nav className="fixed left-0 top-14 hidden w-[13%] md:flex">
+            <ScrollArea className="max-h-[calc(100vh - 3.5rem)] flex w-full flex-col gap-4 py-8">
+                {links.map((link) => (
+                    <Link
+                        className={clsx(
+                            {
+                                "hover:bg-accent": pathname !== link.path,
+                                "bg-primary text-primary-foreground": pathname === link.path,
+                            },
+                            "flex flex-row items-center rounded-r-full px-4 py-2 font-medium",
+                        )}
+                        href={link.path}
+                        key={link.slug}
+                    >
+                        {link.icon ?? null}
+                        {link.title}
+                    </Link>
+                ))}
+            </ScrollArea>
         </nav>
     );
 }
