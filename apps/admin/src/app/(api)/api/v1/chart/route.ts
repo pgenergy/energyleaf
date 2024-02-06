fea7beec4fb468d68a07abe73cf27e0b87a4c618
m43i <36700796+m43i@users.noreply.github.com>
2024-02-06 16:42:06 +0100

    fix: remove duplicate code

--- apps/admin/src/app/(api)/api/v1/chart/route.ts
+++ apps/admin/src/app/(api)/api/v1/chart/route.ts
@@ -1,41 +1,6 @@
 import { NextResponse, type NextRequest } from "next/server";
+import { renderChart } from "@/lib/chart/util";
 import type { ChartConfiguration } from "chart.js";
-import { Chart } from "chart.js/auto";
-import { JSDOM } from "jsdom";
-
-function renderChart(config: ChartConfiguration) {
-    const dom = new JSDOM(`
-        <!DOCTYPE html>
-        <html>
-            <head>
-            </head>
-            <body>
-                <canvas id="canvas" width="720" height="480"></canvas>
-            </body>
-        </html>
-    `);
-
-    const canvas = dom.window.document.getElementById("canvas") as HTMLCanvasElement;
-    const ctx = canvas.getContext("2d");
-
-    if (!ctx) {
-        throw new Error("Could not get canvas context");
-    }
-
-    const chart = new Chart(ctx, {
-        ...config,
-        options: {
-            ...config.options,
-            animation: false,
-            responsive: false,
-        },
-    });
-
-    const data = canvas.toDataURL("image/png");
-    chart.destroy();
-
-    return data;
-}
 
 // eslint-disable-next-line @typescript-eslint/require-await -- required for Next.js API routes even if not async
 export async function GET(req: NextRequest) {
