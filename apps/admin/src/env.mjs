5459a9b93fbb90694f4e39bfb64f1be01d54a623
m43i <36700796+m43i@users.noreply.github.com>
2024-02-07 16:21:14 +0100

    fix: dont use hardcoded url

--- apps/admin/src/env.mjs
+++ apps/admin/src/env.mjs
@@ -3,7 +3,10 @@ import { z } from "zod";
 
 export const env = createEnv({
     server: {
+        NEXTAUTH_URL: z.string().optional(),
         NEXTAUTH_SECRET: z.string(),
+
+        VERCEL_URL: z.string().optional(),
 
         DATABASE_HOST: z.string(),
         DATABASE_USERNAME: z.string(),
