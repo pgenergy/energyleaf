9608fb9bc569009e78fa5e3980398075b402f8e0
m43i <36700796+m43i@users.noreply.github.com>
2024-02-08 13:29:13 +0100

    fix: remove console log

--- apps/web/src/app/(auth)/page.tsx
+++ apps/web/src/app/(auth)/page.tsx
@@ -3,7 +3,6 @@ import LoginForm from "@/components/auth/login-form";
 import { CardContent } from "@energyleaf/ui";
 
 export default function Page() {
-    console.log(process.env.NEXTAUTH_SECRET);
     return (
         <CardContent>
             <LoginForm />
