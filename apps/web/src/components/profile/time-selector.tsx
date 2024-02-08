21981a461963bf3c7d415c915ee9c1c95580d9b3
Oliver <oliver.renze@uni-oldenburg.de>
2024-02-08 11:30:12 +0100

    Increase number of shown entrys of select globally and reduce it manually for the time selector

--- apps/web/src/components/profile/time-selector.tsx
+++ apps/web/src/components/profile/time-selector.tsx
@@ -10,7 +10,7 @@ export default function TimeSelector({field}) {
             <SelectTrigger>
                 <SelectValue placeholder="Uhrzeit auswählen"/>
             </SelectTrigger>
-            <SelectContent>
+            <SelectContent className = "max-h-52">
                 {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23].map((value) => (
                     <SelectItem key={value} value={value.toString()}>
                         {(value + ":00")}
