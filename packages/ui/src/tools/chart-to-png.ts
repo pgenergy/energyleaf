b2c92bd9462e9c6d7308b9a46a91cf2dd3eba96b
m43i <36700796+m43i@users.noreply.github.com>
2024-02-08 13:05:34 +0100

    feat: add doc comment

--- packages/ui/src/tools/chart-to-png.ts
+++ packages/ui/src/tools/chart-to-png.ts
@@ -4,15 +4,22 @@ import { JSDOM } from "jsdom";
 
 /**
  * Renders a chart to a base64 data string that can be used as a src for an image tag.
+ *
+ * @param config - The chart configuration. See https://www.chartjs.org/docs/latest/configuration/
+ * @param width - The width of the chart in pixels.
+ * @param height - The height of the chart in pixels.
+ *
+ * @returns A base64 data string to include in image src tag
+ * @throws If the canvas context could not be obtained from the JSDOM instance
  */
-export function renderChart(config: ChartConfiguration) {
+export function renderChart(config: ChartConfiguration, width: number, height: number) {
     const dom = new JSDOM(`
         <!DOCTYPE html>
         <html>
             <head>
             </head>
             <body>
-                <canvas id="canvas" width="720" height="480"></canvas>
+                <canvas id="canvas" width="${width}" height="${height}"></canvas>
             </body>
         </html>
     `);
