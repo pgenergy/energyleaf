6467c7cbd06cfeb5d7f02d11c79f08b0ed49b884
m43i <36700796+m43i@users.noreply.github.com>
2024-02-08 13:25:27 +0100

    fix: auth requires SECRET set

--- apps/web/src/app/(auth)/page.tsx
+++ apps/web/src/app/(auth)/page.tsx
@@ -3,6 +3,7 @@ import LoginForm from "@/components/auth/login-form";
 import { CardContent } from "@energyleaf/ui";
 
 export default function Page() {
+    console.log(process.env.NEXTAUTH_SECRET);
     return (
         <CardContent>
             <LoginForm />
