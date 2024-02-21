54de523823e20c618b85ab59d95b0673f169f146
Oliver <oliver.renze@uni-oldenburg.de>
2024-02-21 14:22:36 +0100

    Remove test tables to cut it out of demo

--- apps/web/src/app/(site)/report/page.tsx
+++ apps/web/src/app/(site)/report/page.tsx
@@ -1,8 +1,5 @@
-import {Card, CardContent, CardDescription, CardHeader, CardTitle, Skeleton} from "@energyleaf/ui";
+import {Card, CardContent, CardDescription, CardHeader, CardTitle} from "@energyleaf/ui";
 import React, {Suspense} from "react";
-import AvgEnergyConsumptionCard from "@/components/recommendations/avg-energy-consumption-card";
-import AvgEnergyConsumptionComparisonCard from "@/components/recommendations/avg-energy-consumption-comparison";
-import DueUsersTable from "@/components/report/temp-dueUsers-table";
 
 
 export default function ReportPage() {
@@ -10,14 +7,9 @@ export default function ReportPage() {
         <div className="flex flex-col gap-4">
             <Card className="w-full">
                 <CardHeader>
-                    <CardTitle>Fällige User für Daily-Mail</CardTitle>
-                    <CardDescription>Hier siehst du übergangsweise alle User, bei denen ein Report fällig ist</CardDescription>
+                    <CardTitle>Berichte</CardTitle>
+                    <CardDescription>Hier siehst du zukünftig deine vergangenen Berichte</CardDescription>
                 </CardHeader>
-                <CardContent>
-                    <div className="flex flex-row justify-center">
-                        <DueUsersTable/>
-                    </div>
-                </CardContent>
             </Card>
         </div>
     );
