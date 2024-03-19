25ab701029c293baf2cad722965cb722a8ebba5f
Daniel Tapken <danieltapken32@gmail.com>
2024-03-19 10:38:29 +0100

    Fix energy costs card in demo
    Timestamp was of type string which lead to issues when functions of date were called

--- apps/web/src/query/user.ts
+++ apps/web/src/query/user.ts
@@ -1,9 +1,9 @@
-import { cache } from "react";
+import {cache} from "react";
 
 import "server-only";
 
-import { cookies } from "next/headers";
-import { getUserDataCookieStore } from "@/lib/demo/demo";
+import {cookies} from "next/headers";
+import {getUserDataCookieStore} from "@/lib/demo/demo";
 
 import {
     getUserById as getDbUserById,
@@ -40,7 +40,10 @@ export const getUserData = cache(async (id: string) => {
             return getUserDataCookieStore();
         }
 
-        return JSON.parse(data) as UserDataType;
+        return JSON.parse(
+            data,
+            (key, value) => key === "timestamp" ? new Date(value) : value
+        ) as UserDataType;
     }
     return getDbUserDataById(id);
 });
