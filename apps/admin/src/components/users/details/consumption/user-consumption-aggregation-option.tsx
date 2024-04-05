c06a1b1c186e717fe5f24622773b0f7a15282467
Hanno <hanno.kuehlke@uni-oldenburg.de>
2024-04-05 16:17:38 +0200

    Refactor aggregation options based on date range

--- apps/admin/src/components/users/details/consumption/user-consumption-aggregation-option.tsx
+++ apps/admin/src/components/users/details/consumption/user-consumption-aggregation-option.tsx
@@ -1,10 +1,39 @@
 "use client";
 
+import React, { useMemo } from "react";
 import { useUserContext } from "@/hooks/user-hook";
-
 import { AggregationOption } from "@energyleaf/ui/components/utils";
+import { AggregationType } from "@energyleaf/lib";
+import { differenceInCalendarDays, differenceInMonths, differenceInWeeks, differenceInYears } from "date-fns";
 
 export default function UserConsumptionAggregationOption() {
     const context = useUserContext();
-    return <AggregationOption onSelectedChange={context.setAggregationType} selected={context.aggregationType} />;
+
+    const availableOptions = useMemo(() => {
+        const startDate = new Date(context.startDate);
+        const endDate = new Date(context.endDate);
+
+        const dayDiff = differenceInCalendarDays(endDate, startDate);
+        const monthDiff = differenceInMonths(endDate, startDate);
+        const weekDiff = differenceInWeeks(endDate, startDate);
+        const yearDiff = differenceInYears(endDate, startDate);
+
+        let options = [AggregationType.RAW];
+
+        if (dayDiff <= 1) options.push(AggregationType.HOUR);
+        if (dayDiff >= 1 && monthDiff <= 1) options.push(AggregationType.DAY);
+        if (weekDiff >= 1 && monthDiff <= 3) options.push(AggregationType.WEEK);
+        if (monthDiff >= 1 && yearDiff <= 1) options.push(AggregationType.MONTH);
+        if (yearDiff >= 1) options.push(AggregationType.YEAR);
+
+        return options;
+    }, [context.startDate, context.endDate]);
+
+    return (
+        <AggregationOption
+            availableOptions={availableOptions}
+            onSelectedChange={context.setAggregationType}
+            selected={context.aggregationType}
+        />
+    );
 }
--- packages/ui/src/components/utils/aggregation-option.tsx
+++ packages/ui/src/components/utils/aggregation-option.tsx
@@ -1,15 +1,23 @@
 import React from "react";
-
 import { AggregationType } from "@energyleaf/lib";
-
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "../../ui";
 
 interface Props {
     selected?: AggregationType;
     onSelectedChange: (selected: AggregationType) => void;
+    availableOptions: AggregationType[];
 }
 
-export function AggregationOption({ selected, onSelectedChange: onChange }: Props) {
+const aggregationLabels = {
+    [AggregationType.RAW]: "Nicht aggregiert",
+    [AggregationType.HOUR]: "Stunde",
+    [AggregationType.DAY]: "Tag",
+    [AggregationType.WEEK]: "Woche",
+    [AggregationType.MONTH]: "Monat",
+    [AggregationType.YEAR]: "Jahr",
+};
+
+export function AggregationOption({ selected, onSelectedChange: onChange, availableOptions }: Props) {
     return (
         <div className="flex flex-row justify-end gap-4">
             <Select defaultValue={AggregationType.RAW} onValueChange={onChange} value={selected}>
@@ -17,11 +25,13 @@ export function AggregationOption({ selected, onSelectedChange: onChange }: Prop
                     <SelectValue placeholder="Granularität" />
                 </SelectTrigger>
                 <SelectContent>
-                    <SelectItem value={AggregationType.RAW}>Nicht aggregiert</SelectItem>
-                    <SelectItem value={AggregationType.HOUR}>Stunde</SelectItem>
-                    <SelectItem value={AggregationType.DAY}>Tag</SelectItem>
-                    <SelectItem value={AggregationType.MONTH}>Monat</SelectItem>
-                    <SelectItem value={AggregationType.YEAR}>Jahr</SelectItem>
+                    {Object.values(AggregationType).map((type) =>
+                        availableOptions.includes(type) ? (
+                            <SelectItem key={type} value={type}>
+                                {aggregationLabels[type]}
+                            </SelectItem>
+                        ) : null
+                    )}
                 </SelectContent>
             </Select>
         </div>
