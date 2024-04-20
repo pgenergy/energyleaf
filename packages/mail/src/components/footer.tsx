4957eca4a4ab7f89c7657b67e8e772b2a16ed35b
Lukas Hein <lukashein@protonmail.com>
2024-04-20 17:03:20 +0200

    Reformat code

--- packages/mail/src/components/footer.tsx
+++ packages/mail/src/components/footer.tsx
@@ -7,19 +7,19 @@ export function Footer() {
     return (
         <Container>
             <Hr className="border border-border"/>
-        <Container className="flex flex-row justify-center">
-        <Text className="font-bold">Energyleaf</Text>
+            <Container className="flex flex-row justify-center">
+                <Text className="font-bold">Energyleaf</Text>
             </Container>
             <Container className="flex flex-row justify-center p-0">
-    <Text>
-        <Link
-            className="hover:underline"
-    href={`${baseUrl}/privacy`}
-    rel="noopener"
-    target="_blank"
-        >
-        Datenschutz
-        </Link> | <Link className="hover:underline" href={`${baseUrl}/legal`}
+                <Text>
+                    <Link
+                        className="hover:underline"
+                        href={`${baseUrl}/privacy`}
+                        rel="noopener"
+                        target="_blank"
+                    >
+                        Datenschutz
+                    </Link> | <Link className="hover:underline" href={`${baseUrl}/legal`}
                                     rel="noopener" target="_blank">
                     Impressum
                 </Link>
--- packages/mail/src/components/header.tsx
+++ packages/mail/src/components/header.tsx
@@ -13,8 +13,8 @@ export function Header({children}: Props) {
         <Container>
             <Container className="flex flex-row-reverse">
                 <Link href={baseUrl} className="">
-                <Img src={`${baseUrl}/_next/image?url=%2Fimage%2Flogo%2Flogo_text.png&w=1080&q=75`}
-                     height="72" alt="Energyleaf Logo"/>
+                    <Img src={`${baseUrl}/_next/image?url=%2Fimage%2Flogo%2Flogo_text.png&w=1080&q=75`}
+                         height="72" alt="Energyleaf Logo"/>
                 </Link>
             </Container>
             <h2>{children}</h2>
