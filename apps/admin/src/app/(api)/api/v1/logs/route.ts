121ed0d3e9e6cbcc251bcda6138c7a8290b6cc6b
Oliver <oliver.renze@uni-oldenburg.de>
2024-04-23 09:14:36 +0200

    Try to get format of incoming log drains

--- apps/admin/src/app/(api)/api/v1/logs/route.ts
+++ apps/admin/src/app/(api)/api/v1/logs/route.ts
@@ -1,6 +1,6 @@
 import {NextRequest, NextResponse} from "next/server";
 import {env} from "@/env.mjs";
-import {insertLog} from "@energyleaf/db/query";
+import {insertLog, LogType} from "@energyleaf/db/query";
 
 export const POST = async (req: NextRequest) => {
     const reportApiKey = env.REPORTS_API_KEY;
@@ -8,13 +8,16 @@ export const POST = async (req: NextRequest) => {
         return NextResponse.json({status: 403, statusMessage: "Forbidden"});
     }
 
-    let data = await req.body.json();
-    if (!data) {
-        return NextResponse.json({status: 400, statusMessage: "Bad Request"});
-    }
+    console.log(req)
+    console.log(req.headers)
+    console.log(req.body)
+
+    // if (!data) {
+    //     return NextResponse.json({status: 400, statusMessage: "Bad Request"});
+    // }
 
     try {
-        await insertLog(data);
+        // await insertLog(data);
         return NextResponse.json({status: 200, statusMessage: "Inserted log."});
     } catch (e) {
         return NextResponse.json({status: 500, statusMessage: "Internal Server Error"});
