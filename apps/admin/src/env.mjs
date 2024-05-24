31e2bde9487a6cccb9ed711ce0f065450302bd94
m43i <36700796+m43i@users.noreply.github.com>
2024-05-24 13:20:06 +0200

    fix: add missing env variable from preset

--- apps/admin/src/env.mjs
+++ apps/admin/src/env.mjs
@@ -7,6 +7,8 @@ export const env = createEnv({
         NEXTAUTH_URL: z.string().optional(),
         NEXTAUTH_SECRET: z.string(),
 
+        VERCEL_PROJECT_PRODUCTION_URL: z.string(),
+
         DATABASE_HOST: z.string(),
         DATABASE_USERNAME: z.string(),
         DATABASE_PASSWORD: z.string(),
