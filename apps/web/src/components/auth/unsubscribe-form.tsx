9173dec4398eb5aa8c83ab58ef262d10e4dc5e84
Daniel Tapken <danieltapken32@gmail.com>
2024-06-06 13:44:59 +0200

    Fix unsubscribe form

--- apps/web/src/components/auth/unsubscribe-form.tsx
+++ apps/web/src/components/auth/unsubscribe-form.tsx
@@ -5,8 +5,9 @@ import type { z } from "zod";
 
 import { updateReportConfigSettings } from "@/actions/auth";
 import ReportConfigForm from "@/components/profile/report-config-form";
+import type { reportSettingsSchema } from "@/lib/schema/profile";
+import type { DefaultActionReturn } from "@energyleaf/lib";
 import { useRouter } from "next/navigation";
-import type {reportSettingsSchema} from "@/lib/schema/profile";
 
 interface Props {
     reportConfig: z.infer<typeof reportSettingsSchema>;
@@ -15,15 +16,30 @@ interface Props {
 
 export default function UnsubscribeForm({ reportConfig, userId }: Props) {
     const router = useRouter();
+
+    async function update(data: z.infer<typeof reportSettingsSchema>) {
+        let res: DefaultActionReturn = undefined;
+        try {
+            res = await updateReportConfigSettings(data, userId);
+        } catch (err) {
+            throw new Error("Ein Fehler ist aufgetreten.");
+        }
+
+        if (res && !res?.success) {
+            throw new Error(res?.message);
+        }
+
+        router.push("/unsubscribed");
+    }
+
     function onSubmit(data: z.infer<typeof reportSettingsSchema>) {
-        toast.promise(updateReportConfigSettings(data, userId), {
+        toast.promise(update(data), {
             loading: "Bericht-Einstellungen werden aktualisiert...",
             success: "Bericht-Einstellungen erfolgreich aktualisiert",
             error: (err) => {
-                return "Fehler beim Anmelden";
+                return err.message;
             },
         });
-        router.push("/unsubscribed");
     }
 
     return <ReportConfigForm onSubmit={onSubmit} reportConfig={reportConfig} />;
