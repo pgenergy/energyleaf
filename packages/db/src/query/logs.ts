b5f4adb75b03c87943b19a68ca459c95802469cf
Oliver <oliver.renze@uni-oldenburg.de>
2024-06-12 23:01:34 +0200

    Updated scheme, use same table for technical logs and user action tracking

--- packages/db/src/query/logs.ts
+++ packages/db/src/query/logs.ts
@@ -4,13 +4,46 @@ import { logs } from "../schema/logs";
 export async function log(
     title: string,
     logType: (typeof logs.logType.enumValues)[number],
+    appFunction: string,
     appComponent: (typeof logs.appComponent.enumValues)[number],
-    details: string,
+    details: object,
 ) {
     await db.insert(logs).values({
         title: title,
         logType: logType,
+        appFunction: appFunction,
         appComponent: appComponent,
         details: details,
     });
+}
+
+export async function logError(
+    title: string,
+    appFunction: string,
+    appComponent: (typeof logs.appComponent.enumValues)[number],
+    details: object,
+    error: Error,
+) {
+    const detailsObj = {
+        ...details,
+        error: {
+            name: error.name,
+            message: error.message,
+            stack: error.stack?.split("\n").slice(1, 4).join("\n"),
+        }
+    };
+
+    //Important todo -> Use blob function to store error stack
+
+    console.error(error);
+    await log(title, "error", appFunction, appComponent, detailsObj);
+}
+
+export async function trackAction(
+    title: string,
+    appFunction: string,
+    appComponent: (typeof logs.appComponent.enumValues)[number],
+    details: object,
+) {
+    await log(title, "action", appFunction, appComponent, details);
 }
--- packages/db/src/schema/logs.ts
+++ packages/db/src/schema/logs.ts
@@ -1,11 +1,12 @@
 import { sql } from "drizzle-orm";
-import { int, mysqlEnum, mysqlTable, timestamp, varchar } from "drizzle-orm/mysql-core";
+import {int, json, mysqlEnum, mysqlTable, timestamp, varchar} from "drizzle-orm/mysql-core";
 
 export const logs = mysqlTable("logs", {
     id: int("id").autoincrement().primaryKey().notNull(),
     timestamp: timestamp("timestamp").default(sql`CURRENT_TIMESTAMP`),
-    title: varchar("title", {length: 1024}).notNull(),
-    logType: mysqlEnum("log_type", ["info", "error", "undefined"]).default("undefined"),
+    title: varchar("title", {length: 128}).notNull(),
+    logType: mysqlEnum("log_type", ["action", "error", "info", "undefined"]).default("undefined"),
+    appFunction: varchar("app_function", {length: 128}).notNull(),
     appComponent: mysqlEnum("app_component", ["web", "admin", "api", "undefined"]).default("undefined"),
-    details: varchar("content", {length: 12000}),
+    details: json("details").$type<unknown>(),
 });
