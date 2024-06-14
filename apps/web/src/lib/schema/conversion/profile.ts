10619164dd64ad74dd508a82a7229079cf5d5cfc
Daniel Tapken <danieltapken32@gmail.com>
2024-06-14 14:07:18 +0200

    Fix bug that time 0 was not loaded

--- apps/web/src/lib/schema/conversion/profile.ts
+++ apps/web/src/lib/schema/conversion/profile.ts
@@ -4,22 +4,22 @@ import type { mailSettingsSchema } from "../profile";
 
 export function createUserDataSchemaFromUserDataSelectType(userData: UserDataSelectType | null) {
     return {
-        houseType: userData?.property || "house",
-        livingSpace: userData?.livingSpace || 0,
-        people: userData?.household || 0,
-        hotWater: userData?.hotWater || "electric",
-        tariff: userData?.tariff || "basic",
-        basePrice: userData?.basePrice || 0,
-        workingPrice: userData?.workingPrice || 0,
-        monthlyPayment: userData?.monthlyPayment || 0,
+        houseType: userData?.property ?? "house",
+        livingSpace: userData?.livingSpace ?? 0,
+        people: userData?.household ?? 0,
+        hotWater: userData?.hotWater ?? "electric",
+        tariff: userData?.tariff ?? "basic",
+        basePrice: userData?.basePrice ?? 0,
+        workingPrice: userData?.workingPrice ?? 0,
+        monthlyPayment: userData?.monthlyPayment ?? 0,
     };
 }
 
 export function createMailSettingsSchema(mailConfig: MailConfig | null): z.infer<typeof mailSettingsSchema> {
     return {
-        interval: mailConfig?.report_config.interval || 3,
-        receiveReportMails: mailConfig?.report_config.receiveMails || false,
-        time: mailConfig?.report_config.time || 6,
-        receiveAnomalyMails: mailConfig?.anomaly_config.receiveMails || false,
+        interval: mailConfig?.report_config.interval ?? 3,
+        receiveReportMails: mailConfig?.report_config.receiveMails ?? false,
+        time: mailConfig?.report_config.time ?? 6,
+        receiveAnomalyMails: mailConfig?.anomaly_config.receiveMails ?? false,
     };
 }
