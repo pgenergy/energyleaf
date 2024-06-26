b6813a1fc6e266e2ff7baee0b1bf363dff1b8fb8
Oliver <oliver.renze@uni-oldenburg.de>
2024-06-26 13:44:33 +0200

    Neue allgemeine FAQs hinzugefügt

--- apps/web/src/app/(site)/faq/page.tsx
+++ apps/web/src/app/(site)/faq/page.tsx
@@ -33,16 +33,40 @@ export default async function FaqPage() {
                             </AccordionContent>
                         </AccordionItem>
                         <AccordionItem value="item-2">
+                            <AccordionTrigger>Was ist der Hintergrund von Energyleaf?</AccordionTrigger>
+                            <AccordionContent>
+                                Energyleaf ist aus der Forschungsarbeit von Prof. Dr. Philipp Staudt entstanden. Er
+                                untersucht die Auswirkung von digitalen Hilfsmitteln auf das Verständnis von
+                                Privatpersonen für ihren eigenen Energieverbrauch. Im Rahmen seiner Forschung betreut er
+                                die Projektgruppe Energyleaf bestehend aus (Wirtschafts-)Informatik-Studierenden der
+                                Universität Oldenburg, die die gleichnamige App entwickelt haben. Mit Ihrer Teilnahme an
+                                Feldexperimenten der Projektgruppe gewinnen Sie also nicht nur Einblick in Ihren
+                                Stromverbrauch, sondern unterstützen auch die Forschungsarbeit der Carl von Ossietzky
+                                Universität und helfen den Studierenden bei der Evaluation ihres Softwareprojekts.
+                            </AccordionContent>
+                        </AccordionItem>
+                        <AccordionItem value="item-3">
+                            <AccordionTrigger>
+                                An wen kann ich mich bei Fragen zur Energyleaf-App wenden?
+                            </AccordionTrigger>
+                            <AccordionContent>
+                                Wenn Sie Anmerkungen oder Fragen zur Anwendung haben, kontaktieren Sie die
+                                verantwortliche Projektgruppe unter energyleaf@uni-oldenburg.de. Das dahinterstehende
+                                Forschungsprojekt stammt von Prof. Dr. Philipp Staudt, den Sie unter
+                                philipp.staudt@uni-oldenburg.de erreichen können.
+                            </AccordionContent>
+                        </AccordionItem>
+                        <AccordionItem value="item-4">
                             <AccordionTrigger>
                                 Wie kann ich meinen aktuellen Energieverbrauch in Echtzeit überwachen?
                             </AccordionTrigger>
                             <AccordionContent>
                                 Unter „Übersicht“ sehen Sie Ihren aktuellen Energieverbrauch in Kilowattstunden. Das
                                 Liniendiagramm zeigt den Verlauf Ihres Verbrauchs, und Sie können die Daten auch im
-                                CSV-Format exportieren. Spitzenverbrauch wird mit roten Punkten markiert.
+                                CSV-Format exportieren. Der Spitzenverbrauch wird mit roten Punkten markiert.
                             </AccordionContent>
                         </AccordionItem>
-                        <AccordionItem value="item-3">
+                        <AccordionItem value="item-5">
                             <AccordionTrigger>
                                 Wie kann ich meine Profilinformationen in Energyleaf aktualisieren?
                             </AccordionTrigger>
@@ -52,7 +76,7 @@ export default async function FaqPage() {
                                 Maileinstellungen anpassen.
                             </AccordionContent>
                         </AccordionItem>
-                        <AccordionItem value="item-4">
+                        <AccordionItem value="item-6">
                             <AccordionTrigger>
                                 Kann ich regelmäßig Berichte über meinen Energieverbrauch erhalten?
                             </AccordionTrigger>
@@ -64,18 +88,18 @@ export default async function FaqPage() {
                         </AccordionItem>
                         {fulfills(user.appVersion, Versions.self_reflection) && (
                             <div>
-                                <AccordionItem value="item-5">
+                                <AccordionItem value="item-7">
                                     <AccordionTrigger>
                                         Wie erhalte ich Benachrichtigungen bei hohem Energieverbrauch?
                                     </AccordionTrigger>
                                     <AccordionContent>
                                         Energyleaf sendet Ihnen bei aktivierter Einstellung automatisch eine
                                         Benachrichtigung per E-Mail, wenn Ihr aktueller Verbrauch stark von Ihrem
-                                        normalen Verbrauch abweicht und ungewöhnlich hoch ist. Sollten die Einstellung
-                                        aktivieren wollen, können Sie dies oben rechts in Ihrem Profil tun.
+                                        normalen Verbrauch abweicht und ungewöhnlich hoch ist. Sollten Sie die
+                                        Einstellung aktivieren wollen, können Sie dies oben rechts in Ihrem Profil tun.
                                     </AccordionContent>
                                 </AccordionItem>
-                                <AccordionItem value="item-6">
+                                <AccordionItem value="item-8">
                                     <AccordionTrigger>
                                         Wie kann ich mein Energieverbrauchsziel festlegen und überwachen?
                                     </AccordionTrigger>
@@ -86,7 +110,7 @@ export default async function FaqPage() {
                                         Sie, wenn Sie es überschreiten.
                                     </AccordionContent>
                                 </AccordionItem>
-                                <AccordionItem value="item-7">
+                                <AccordionItem value="item-9">
                                     <AccordionTrigger>
                                         Wie kann ich mein Energieverbrauchsziel festlegen und überwachen?
                                     </AccordionTrigger>
@@ -99,7 +123,7 @@ export default async function FaqPage() {
                                 </AccordionItem>
                             </div>
                         )}
-                        <AccordionItem value="item-8">
+                        <AccordionItem value="item-10">
                             <AccordionTrigger>Wo finde ich weitere Stromsparhilfen?</AccordionTrigger>
                             <AccordionContent>
                                 Weitere Stromsparhilfen finden Sie zum Beispiel beim Stromsparcheck:{" "}
@@ -113,7 +137,7 @@ export default async function FaqPage() {
                                 </a>
                             </AccordionContent>
                         </AccordionItem>
-                        <AccordionItem value="item-9">
+                        <AccordionItem value="item-11">
                             <AccordionTrigger>
                                 Wo kann ich sehen, zu welchen Zeiten viel Wind- und Sonnenenergie zur Verfügung steht?
                             </AccordionTrigger>
