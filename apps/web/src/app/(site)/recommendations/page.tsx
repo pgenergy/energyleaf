21d6eba1f9ccab5127c1965069420ed8db83289b
Oliver <oliver.renze@uni-oldenburg.de>
2024-07-18 13:38:11 +0200

    remove no longer used components

--- apps/web/src/app/(site)/recommendations/page.tsx
+++ apps/web/src/app/(site)/recommendations/page.tsx
@@ -1,6 +1,4 @@
 import AmortizationCard from "@/components/recommendations/amortization-card";
-import AvgEnergyConsumptionComparisonCard from "@/components/recommendations/avg-energy-consumption-comparison";
-import AvgEnergyConsumptionComparisonError from "@/components/recommendations/avg-energy-consumption-comparison-error";
 import { ErrorBoundary } from "@energyleaf/ui/error";
 import { Skeleton } from "@energyleaf/ui/skeleton";
 import { Suspense } from "react";
@@ -12,15 +10,6 @@ export const metadata = {
 export default function RecommendationsPage() {
     return (
         <div className="flex flex-col gap-4">
-            <div className="grid grid-cols-1 gap-4 md:grid-cols-3">
-                <div>
-                    <ErrorBoundary fallback={AvgEnergyConsumptionComparisonError}>
-                        <Suspense fallback={<Skeleton className="h-72 w-full" />}>
-                            <AvgEnergyConsumptionComparisonCard />
-                        </Suspense>
-                    </ErrorBoundary>
-                </div>
-            </div>
             <AmortizationCard />
         </div>
     );
