33abb2f5c491f3103bbdb018feb9c229b2cba559
m43i <36700796+m43i@users.noreply.github.com>
2024-07-21 17:54:21 +0200

    refactor(style): run format

--- apps/web/src/app/(site)/costs/page.tsx
+++ apps/web/src/app/(site)/costs/page.tsx
@@ -75,7 +75,7 @@ export default async function CostsPage() {
             {/* Absolute Energiekosten */}
             <section>
                 <h2 className="mb-4 font-bold text-xl">Absolute Energiekosten</h2>
-                <div className="grid grid-cols-1 gap-4 lg:grid-cols-4 md:grid-cols-2">
+                <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-4">
                     <ErrorBoundary fallback={EnergyCostsTodayError}>
                         <Suspense fallback={<Skeleton className="h-40 w-full" />}>
                             <EnergyCostsToday userData={userData} energyDataRaw={energyDataRaw} />
@@ -102,7 +102,7 @@ export default async function CostsPage() {
             {/* Durchschnittliche Energiekosten */}
             <section>
                 <h2 className="mb-4 font-bold text-xl">Durchschnittliche Energiekosten</h2>
-                <div className="grid grid-cols-1 gap-4 lg:grid-cols-3 md:grid-cols-2">
+                <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
                     <Suspense fallback={<Skeleton className="h-40 w-full" />}>
                         <AverageEnergyCostsDay userData={userData} energyData={energyDataRaw} />
                     </Suspense>
@@ -131,7 +131,7 @@ export default async function CostsPage() {
             {/* Energiekosten Vergleiche */}
             <section>
                 <h2 className="mb-4 font-bold text-xl">Energiekosten Vergleiche</h2>
-                <div className="grid grid-cols-1 gap-4 lg:grid-cols-4 md:grid-cols-2">
+                <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-4">
                     <ErrorBoundary fallback={EnergyCostsChangeLastSevenDaysError}>
                         <Suspense fallback={<Skeleton className="h-40 w-full" />}>
                             <EnergyCostsChangeLastWeek userData={userData} energyData={energyDataRaw} />
@@ -158,7 +158,7 @@ export default async function CostsPage() {
             {/* Hochrechnungen */}
             <section>
                 <h2 className="mb-4 font-bold text-xl">Hochrechnungen</h2>
-                <div className="grid grid-cols-1 gap-4 lg:grid-cols-3 md:grid-cols-2">
+                <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
                     <Suspense fallback={<Skeleton className="h-40 w-full" />}>
                         <EnergyCostsProjectionDay userData={userData} energyData={energyDataRaw} />
                     </Suspense>
