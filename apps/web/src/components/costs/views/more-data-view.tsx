5d486121e843b32646d558f5fe6e0df570b52ca1
m43i <36700796+m43i@users.noreply.github.com>
2024-07-29 20:50:44 +0200

    refactor: split more cost data

--- apps/web/src/components/costs/views/more-data-view.tsx
+++ apps/web/src/components/costs/views/more-data-view.tsx
@@ -6,13 +6,14 @@ import ThrieftiestCostCard from "../ui/thrieftiest-cost-card";
 export default function CostMoreDataView() {
     return (
         <>
-            <h2 className="col-span-1 font-bold text-xl md:col-span-3">Weitere Daten</h2>
+            <h2 className="col-span-1 font-bold text-xl md:col-span-3">Durchschnittliche Kosten</h2>
             <Suspense fallback={<Skeleton className="h-40 w-full" />}>
                 <CostAverageCard agg="day" />
             </Suspense>
             <Suspense fallback={<Skeleton className="h-40 w-full" />}>
                 <CostAverageCard agg="week" />
             </Suspense>
+            <h2 className="col-span-1 font-bold text-xl md:col-span-3">Sparsamste Kosten</h2>
             <Suspense fallback={<Skeleton className="h-40 w-full" />}>
                 <ThrieftiestCostCard agg="day" />
             </Suspense>
