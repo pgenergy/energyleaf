5ebdf1935767c0c27bf4e8ac237ac6f6d98d2188
Daniel Tapken <danieltapken32@gmail.com>
2024-07-30 15:20:43 +0200

    Fix stuff

---
+++ apps/web/src/components/recommendations/tips/energy-tip-card-description.tsx
@@ -0,0 +1,65 @@
+import DeviceCategoryIcon from "@/components/devices/device-category-icon";
+import {
+    DeviceCategory,
+    DeviceCategoryTitles,
+    DeviceSuperCategory,
+    DeviceSuperCategoryTitles,
+} from "@energyleaf/db/types";
+import type { EnergyTip } from "@energyleaf/lib/tips";
+import { CardDescription } from "@energyleaf/ui/card";
+import { useMemo } from "react";
+
+interface Props {
+    tip: EnergyTip;
+}
+
+export default function EnergyTipCardDescription({ tip }: Props) {
+    const { belongsTo } = tip;
+
+    const content = useMemo(() => {
+        if (!belongsTo) {
+            return "Allgemein";
+        }
+
+        function isDeviceCategory(
+            value: DeviceCategory | DeviceCategory[] | DeviceSuperCategory,
+        ): value is DeviceCategory {
+            return typeof value === "string" && Object.values(DeviceCategory).includes(value as DeviceCategory);
+        }
+
+        function isDeviceCategoryArray(
+            value: DeviceCategory | DeviceCategory[] | DeviceSuperCategory,
+        ): value is DeviceCategory[] {
+            return Array.isArray(value) && value.every(isDeviceCategory);
+        }
+
+        function isDeviceSuperCategory(
+            value: DeviceCategory | DeviceCategory[] | DeviceSuperCategory,
+        ): value is DeviceSuperCategory {
+            return (
+                typeof value === "string" && Object.values(DeviceSuperCategory).includes(value as DeviceSuperCategory)
+            );
+        }
+
+        if (isDeviceCategory(belongsTo)) {
+            return (
+                <span className="flex flex-row items-center gap-1">
+                    {`Für Geräte-Kategorie ${DeviceCategoryTitles[belongsTo]}`}
+                    <DeviceCategoryIcon category={belongsTo} />
+                </span>
+            );
+        }
+
+        if (isDeviceCategoryArray(belongsTo)) {
+            return `Für Geräte-Kategorien ${belongsTo.map((category) => DeviceCategoryTitles[category]).join(", ")}`;
+        }
+
+        if (isDeviceSuperCategory(belongsTo)) {
+            return `Für Geräte-Oberkategorie ${DeviceSuperCategoryTitles[belongsTo]}`;
+        }
+
+        return null;
+    }, [belongsTo]);
+
+    return <CardDescription>{content}</CardDescription>;
+}
