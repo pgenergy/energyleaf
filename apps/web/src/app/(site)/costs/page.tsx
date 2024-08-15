b261fc1ddb42e40c83e51d19814252cc6f73b55e
m43i <36700796+m43i@users.noreply.github.com>
2024-08-15 13:31:53 +0200

    fix: set maxDuration on page level

--- apps/web/src/app/(site)/costs/page.tsx
+++ apps/web/src/app/(site)/costs/page.tsx
@@ -1,5 +1,7 @@
 import CostPageTodayView from "@/components/costs/views/today-view";
 
+export const maxDuration = 120;
+
 export default function CostsPage() {
     return <CostPageTodayView />;
 }
