4a5c33cc5f55c044440f7be2d16f6593e2ef260e
m43i <36700796+m43i@users.noreply.github.com>
2024-09-02 20:19:37 +0200

    fix: auto increment

--- packages/postgres/src/schema/logs.ts
+++ packages/postgres/src/schema/logs.ts
@@ -1,12 +1,14 @@
 import { sql } from "drizzle-orm";
-import { integer, json, pgTable, text, timestamp, varchar } from "drizzle-orm/pg-core";
+import { integer, json, pgTable, text, timestamp } from "drizzle-orm/pg-core";
 
 export const logsTable = pgTable("logs", {
-    id: integer("id").primaryKey().notNull().generatedAlwaysAsIdentity(),
+    id: integer("id").primaryKey().notNull().generatedAlwaysAsIdentity({
+        startWith: 81597
+    }),
     timestamp: timestamp("timestamp", { mode: "date", withTimezone: true }).default(sql`now()`),
-    title: varchar("title", { length: 128 }).notNull(),
+    title: text("title").notNull(),
     logType: text("log_type", { enum: ["action", "error", "info", "undefined"] }).default("undefined"),
-    appFunction: varchar("app_function", { length: 128 }).notNull(),
+    appFunction: text("app_function").notNull(),
     appComponent: text("app_component", { enum: ["web", "admin", "api", "undefined"] }).default("undefined"),
     details: json("details").$type<unknown>(),
 });
