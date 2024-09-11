e8e65cd67310fcc371c2d4a8fa3600d8dd1aeb87
m43i <36700796+m43i@users.noreply.github.com>
2024-09-11 13:57:42 +0200

    fix: right url

--- apps/web/src/components/nav/navbar-avatar.tsx
+++ apps/web/src/components/nav/navbar-avatar.tsx
@@ -106,7 +106,7 @@ export default function NavbarAvatar({ user, guide }: Props) {
                 <DropdownMenuSeparator />
                 {guide ? (
                     <DropdownMenuItem asChild className="cursor-pointer">
-                        <Link href="/faq">
+                        <Link href={guide}>
                             <BookOpenIcon className="mr-2 h-4 w-4" />
                             Handbuch
                         </Link>
