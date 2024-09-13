1526358ba49a2374a53471bab8c1a42095493609
Lukas Hein <lukashein@protonmail.com>
2024-09-13 13:33:41 +0200

    rm debug code

--- apps/web/src/components/dashboard/energy-cost-card.tsx
+++ apps/web/src/components/dashboard/energy-cost-card.tsx
@@ -1,3 +1,4 @@
+import { calculateCosts } from "@/components/dashboard/energy-cost";
 import { getSession } from "@/lib/auth/auth.server";
 import { getElectricitySensorIdForUser, getEnergyDataForSensor } from "@/query/energy";
 import { getUserDataHistory } from "@/query/user";
@@ -41,11 +42,7 @@ export default async function EnergyCostCard({ startDate, endDate }: Props) {
 
     const userData = await getUserDataHistory(userId);
 
-    const { totalCost, totalWorkingCost, totalBasePrice } = {
-        totalCost: 0.69,
-        totalWorkingCost: 0.1,
-        totalBasePrice: 0.3,
-    }; //calculateCosts(userData, energyDataRaw);
+    const { totalCost, totalWorkingCost, totalBasePrice } = calculateCosts(userData, energyDataRaw);
     const parsedTotalCost = Number.parseFloat(totalCost.toFixed(2));
     const parsedTotalWorkingCost = Number.parseFloat(totalWorkingCost.toFixed(2));
     const parsedTotalBaseCost = Number.parseFloat(totalBasePrice.toFixed(2));
