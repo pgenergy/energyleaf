21ba960d63136ee1bf53970b9b060745e9b0c0af
Big0x44 <danieltapken32@gmail.com>
2024-09-19 08:22:48 +0200

    Add reports to live demo in the most hacky way

--- scripts/live-demo-data.ts
+++ scripts/live-demo-data.ts
@@ -2,12 +2,35 @@ import { Versions } from "@energyleaf/lib/versioning";
 import { db, genId } from "@energyleaf/postgres";
 import { createDevices, saveDeviceSuggestionsToPeakDb } from "@energyleaf/postgres/query/device";
 import { findAndMark, getSequencesBySensor } from "@energyleaf/postgres/query/peaks";
-import { createUser, getUserByMail, setUserActive, updateUser } from "@energyleaf/postgres/query/user";
-import { sensorDataTable, sensorTable } from "@energyleaf/postgres/schema/sensor";
-import { DeviceCategory, DeviceCategoryTitles, SensorType } from "@energyleaf/postgres/types";
-import { eq, max, min } from "drizzle-orm";
+import {
+    createUser,
+    getUserById,
+    getUserByMail,
+    getUserDataByUserId,
+    setUserActive,
+    updateUser,
+    updateUserData,
+} from "@energyleaf/postgres/query/user";
+import { sensorDataDayTable, sensorDataTable, sensorTable } from "@energyleaf/postgres/schema/sensor";
+import { DeviceCategory, DeviceCategoryTitles, SensorType, UserDataSelectType } from "@energyleaf/postgres/types";
+import { eq, max, min, sql } from "drizzle-orm";
 import { Argon2id } from "oslo/password";
 import { insertEnergyData } from "./insert-energy-data";
+import { getLastReportForUser, saveReport } from "@energyleaf/postgres/query/report";
+import {
+    convertTZDate,
+    DailyConsumption,
+    DailyGoalProgress,
+    DailyGoalStatistic,
+    formatDate,
+    formatNumber,
+    ReportProps,
+} from "@energyleaf/lib";
+import { getElectricitySensorIdForUser } from "@energyleaf/postgres/query/sensor";
+import { getDayEnergyForSensorInRange, getEnergySumForSensorInRange } from "@energyleaf/postgres/query/energy-get";
+import type { ChartConfiguration, ChartOptions } from "chart.js";
+import { renderChart } from "@energyleaf/lib/utils/chart-to-png";
+import ChartDataLabels from "chartjs-plugin-datalabels";
 
 export async function addLiveDemoData() {
     const userMail = "team@energyleaf.de";
@@ -37,6 +60,30 @@ export async function addLiveDemoData() {
     await updateUser(
         {
             appVersion: Versions.support,
+            onboardingCompleted: true,
+        },
+        userId,
+    );
+
+    await updateUserData(
+        {
+            property: "house",
+            livingSpace: 100,
+            household: 2,
+            hotWater: "electric",
+            tariff: "basic",
+            basePrice: 30,
+            monthlyPayment: 120,
+            workingPrice: 0.38,
+            timestamp: new Date(2021, 1, 1),
+            consumptionGoal: 45,
+            electricityMeterNumber: "demo_number",
+            electricityMeterType: "digital",
+            electricityMeterImgUrl: null,
+            wifiAtElectricityMeter: true,
+            powerAtElectricityMeter: true,
+            installationComment: null,
+            devicePowerEstimationRSquared: null,
         },
         userId,
     );
@@ -109,6 +156,13 @@ export async function addLiveDemoData() {
         }
     }
 
+    await db.execute(sql`CALL refresh_continuous_aggregate('sensor_data_hour', NULL, NULL);`);
+    await db.execute(sql`CALL refresh_continuous_aggregate('sensor_data_day', NULL, NULL);`);
+    await db.execute(sql`CALL refresh_continuous_aggregate('sensor_data_week', NULL, NULL);`);
+    await db.execute(sql`CALL refresh_continuous_aggregate('sensor_data_month', NULL, NULL);`);
+
+    await addReports(userId);
+
     console.log("Live demo data successfully added.");
 }
 
@@ -130,4 +184,271 @@ function splitInto30MinIntervals(startDate: Date, endDate: Date): [Date, Date][]
     }
 
     return intervals;
+}
+
+async function addReports(userId: string) {
+    const user = await getUserById(userId);
+    if (!user) {
+        return;
+    }
+
+    let serverDateFrom = new Date();
+    serverDateFrom.setDate(serverDateFrom.getDate() - 6);
+    serverDateFrom.setHours(0, 0, 0, 0);
+    let dateFrom = convertTZDate(serverDateFrom);
+    let serverDateTo = new Date();
+    serverDateTo.setDate(serverDateTo.getDate() - 4);
+    serverDateTo.setHours(23, 59, 59, 999);
+    let dateTo = convertTZDate(serverDateTo);
+
+    let reportData = await createReportData(
+        {
+            appVersion: user.appVersion,
+            interval: 3,
+            userId: user.id,
+            userName: user.username,
+            email: user.email,
+            receiveMails: true,
+        },
+        dateFrom,
+        dateTo,
+    );
+    await saveReport(reportData, userId);
+
+    serverDateFrom = new Date();
+    serverDateFrom.setDate(serverDateFrom.getDate() - 3);
+    serverDateFrom.setHours(0, 0, 0, 0);
+    dateFrom = convertTZDate(serverDateFrom);
+    serverDateTo = new Date();
+    serverDateTo.setDate(serverDateTo.getDate() - 1);
+    serverDateTo.setHours(23, 59, 59, 999);
+    dateTo = convertTZDate(serverDateTo);
+
+    reportData = await createReportData(
+        {
+            appVersion: user.appVersion,
+            interval: 3,
+            userId: user.id,
+            userName: user.username,
+            email: user.email,
+            receiveMails: true,
+        },
+        dateFrom,
+        dateTo,
+    );
+    await saveReport(reportData, userId);
+}
+
+interface UserReportData {
+    userId: string;
+    userName: string;
+    appVersion: Versions;
+    email: string;
+    receiveMails: boolean;
+    interval: number;
+}
+
+export async function createReportData(user: UserReportData, dateFrom: Date, dateTo: Date): Promise<ReportProps> {
+    const sensor = await getElectricitySensorIdForUser(user.userId);
+    if (!sensor) {
+        throw new Error("No electricity sensor found for User");
+    }
+
+    const userData = await getUserDataByUserId(user.userId);
+    if (!userData) {
+        throw new Error("No user data found for User");
+    }
+
+    const totalEnergyConsumption = await getEnergySumForSensorInRange(dateFrom, dateTo, sensor);
+
+    const avgEnergyConsumption = totalEnergyConsumption / user.interval;
+    const workingPrice = userData.workingPrice;
+    const totalEnergyCost = workingPrice ? totalEnergyConsumption * workingPrice : undefined;
+    const avgEnergyCost = workingPrice ? avgEnergyConsumption * workingPrice : undefined;
+
+    const dailyConsumption: DailyConsumption[] = await getDailyConsumption(sensor, dateFrom, user.interval);
+    const bestDay = dailyConsumption.reduce((prev, current) =>
+        prev.consumption < current.consumption ? prev : current,
+    );
+    const worstDay = dailyConsumption.reduce((prev, current) =>
+        prev.consumption > current.consumption ? prev : current,
+    );
+
+    const dayStatistics: DailyGoalStatistic[] | undefined = await getDailyGoalStatistic(dailyConsumption, userData);
+
+    const dailyConsumptionGraph: string = await renderDailyConsumptionChart(dailyConsumption);
+
+    const lastReport = await getLastReportForUser(user.userId);
+
+    return {
+        userName: user.userName,
+        dateFrom: dateFrom,
+        dateTo: dateTo,
+        dayEnergyStatistics: dayStatistics,
+        totalEnergyConsumption: totalEnergyConsumption,
+        avgEnergyConsumptionPerDay: avgEnergyConsumption,
+        totalEnergyCost: totalEnergyCost,
+        avgEnergyCost: avgEnergyCost,
+        bestDay: bestDay,
+        worstDay: worstDay,
+        lastReport: lastReport ?? undefined,
+        dailyTotalConsumptionGraph: dailyConsumptionGraph,
+    };
+}
+
+async function getDailyConsumption(sensor: string, dateFrom: Date, interval: number): Promise<DailyConsumption[]> {
+    const dates = new Array(interval).fill(null).map((_, index) => {
+        const date = new Date(dateFrom);
+        date.setDate(date.getDate() + index);
+        return date;
+    });
+
+    const tasks: Promise<DailyConsumption>[] = dates.map(async (date) => {
+        const dailyEnergy = await getDayEnergyForSensorInRange(date, date, sensor, "sum");
+        const sumOfDay = dailyEnergy.length > 0 ? (dailyEnergy[0]?.consumption ?? 0) : 0;
+
+        return {
+            day: date,
+            consumption: sumOfDay,
+        };
+    });
+
+    return await Promise.all(tasks);
+}
+
+async function getDailyGoalStatistic(
+    dailyConsumption: DailyConsumption[],
+    userData: UserDataSelectType,
+): Promise<DailyGoalStatistic[]> {
+    const dailyGoalProgresses = await getDailyGoalProgress(dailyConsumption, userData);
+    const promises = dailyGoalProgresses.map(async (day) => {
+        return {
+            ...day,
+            image: await renderDailyStatistic(day),
+        };
+    });
+    return await Promise.all(promises);
+}
+
+async function getDailyGoalProgress(
+    dailyConsumption: DailyConsumption[],
+    userData: UserDataSelectType,
+): Promise<DailyGoalProgress[]> {
+    return dailyConsumption.map((day) => {
+        return {
+            day: day.day,
+            dailyConsumption: day.consumption,
+            dailyGoal: userData.consumptionGoal ?? undefined,
+            exceeded: userData.consumptionGoal ? day.consumption > userData.consumptionGoal : undefined,
+            progress: userData.consumptionGoal ? (day.consumption / userData.consumptionGoal) * 100 : undefined,
+        };
+    });
+}
+
+const primaryColor = "#439869";
+const onPrimaryColor = "#FFFFFF";
+
+function renderDailyConsumptionChart(dayStatistics: DailyConsumption[]) {
+    return renderChart(
+        {
+            type: "bar",
+            data: {
+                labels: dayStatistics.map((x) => formatDate(convertTZDate(x.day, "client"))),
+                datasets: [
+                    {
+                        label: "Täglicher Verbrauch in kWh",
+                        data: dayStatistics.map((x) => x.consumption),
+                        fill: false,
+                        backgroundColor: primaryColor,
+                        tension: 0.1,
+                        datalabels: {
+                            color: onPrimaryColor,
+                            formatter: (value) => formatNumber(value),
+                        },
+                    },
+                ],
+            },
+            plugins: [ChartDataLabels],
+        },
+        600,
+        300,
+    );
+}
+
+export function renderDailyStatistic({ progress, exceeded }: DailyGoalProgress) {
+    const prog = progress ?? 0;
+    const remaining = prog < 100 ? 100 - prog : 0;
+
+    const barColor = exceeded ? "#EF4444" : primaryColor;
+
+    const cutout = 80;
+
+    const chartOptions: ChartOptions<"doughnut"> = {
+        responsive: true,
+        cutout: `${cutout}%`,
+        circumference: 360,
+        plugins: {
+            tooltip: { enabled: false },
+            legend: { display: false },
+        },
+    };
+
+    const chartConfiguration: ChartConfiguration<"doughnut"> = {
+        type: "doughnut",
+        data: {
+            datasets: [
+                {
+                    data: [prog, remaining],
+                    backgroundColor: [barColor, onPrimaryColor],
+                    borderWidth: 0,
+                },
+            ],
+        },
+        options: chartOptions,
+        plugins: [
+            {
+                id: "whiteCenter",
+                beforeDatasetDraw(chart) {
+                    const { ctx } = chart;
+                    ctx.save();
+
+                    const xCenter = chart.getDatasetMeta(0).data[0].x;
+                    const yCenter = chart.getDatasetMeta(0).data[0].y;
+
+                    const startAngle = 0;
+                    const endAngle = 2 * Math.PI;
+                    const radius = 0.6 * cutout; // a bit more than half to prevent a small gap
+
+                    ctx.beginPath();
+                    ctx.arc(xCenter, yCenter, radius, startAngle, endAngle);
+                    ctx.fillStyle = "#f4f4f5";
+                    ctx.fill();
+
+                    ctx.restore();
+                },
+                afterDatasetsDraw(chart) {
+                    const { ctx, data } = chart;
+                    ctx.save();
+
+                    const xCenter = chart.getDatasetMeta(0).data[0].x;
+                    const yCenter = chart.getDatasetMeta(0).data[0].y;
+
+                    ctx.textAlign = "center";
+                    ctx.textBaseline = "middle";
+                    ctx.font = "24px Arial";
+                    ctx.fillStyle = "#000";
+
+                    // Get the percentage from the data
+                    const percentage = Math.round(data.datasets[0].data[0] as number);
+
+                    // Draw the percentage text
+                    ctx.fillText(`${percentage}%`, xCenter, yCenter);
+
+                    ctx.restore();
+                },
+            },
+        ],
+    };
+
+    return renderChart(chartConfiguration as ChartConfiguration, 100, 100);
 }
