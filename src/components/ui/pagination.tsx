e131e59de02e1c133fa906b4bb7bc5e2287d6451
Malte Eiting <malte.eiting@icloud.com>
2025-12-09 14:02:19 +0100

    fix: i18n

--- src/components/ui/pagination.tsx
+++ src/components/ui/pagination.tsx
@@ -57,7 +57,7 @@ function PaginationPrevious({ className, ...props }: React.ComponentProps<typeof
 			{...props}
 		>
 			<ChevronLeftIcon />
-			<span className="hidden sm:block">Previous</span>
+			<span className="hidden sm:block">Zurück</span>
 		</PaginationLink>
 	);
 }
@@ -70,7 +70,7 @@ function PaginationNext({ className, ...props }: React.ComponentProps<typeof Pag
 			className={cn("gap-1 px-2.5 sm:pr-2.5", className)}
 			{...props}
 		>
-			<span className="hidden sm:block">Next</span>
+			<span className="hidden sm:block">Weiter</span>
 			<ChevronRightIcon />
 		</PaginationLink>
 	);
@@ -85,7 +85,7 @@ function PaginationEllipsis({ className, ...props }: React.ComponentProps<"span"
 			{...props}
 		>
 			<MoreHorizontalIcon className="size-4" />
-			<span className="sr-only">More pages</span>
+			<span className="sr-only">Keine weiteren Seiten</span>
 		</span>
 	);
 }
